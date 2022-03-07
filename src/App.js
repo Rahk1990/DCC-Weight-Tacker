e9fa1764062674df8fdf3022ed2b0ae0336690a1
rahk1990 <rahkem.ellis@gmail.com>
2022-03-07 15:03:06 -0500

    added functions to component for document weight and entree date to a form to be used as a componet.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components.jsx/DisplayEntries/DisplayEntries';
+import AddEntryFrom from './Components.jsx/AddEntry.jsx/AddEntryForm';
 
 function App() {
 
-  const[entries, setEntries] = useState([{weight: 175, date: '3-4-2022'}, {weight: 123, date: '3-3-2022'}])
+  const [entries, setEntries] = useState([{weight: 175, date: '3-4-2022'}, {weight: 123, date: '3-3-2022'}])
   
   return (
     <div>
       <DisplayEntries parentEntries={entries}/> 
+      <AddEntryFrom />
+
     </div>
   );
 }
